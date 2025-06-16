42f6cbdf71109c5f10bbeb02d539d1023881fe25
mleila1312 <leilamelody1904@gmail.com>
2025-06-16 16:10:04 +0200

    still doesn't work but a bit better

--- clothes-app/app/excel/page.tsx
+++ clothes-app/app/excel/page.tsx
@@ -3,12 +3,14 @@
 import React, { useState } from "react";
 import { parseCSVText, ColorEntry } from "../../components/color_handling"; // adjust path as needed
 
+
+
 export default function ExcelTest() {
   const [parsedData, setParsedData] = useState<ColorEntry[] | null>(null);
 
   const handleTestStaticCSV = async () => {
     try {
-      const res = await fetch("/public/dataset/data_dyes.csv");
+      const res = await fetch("data_dyes.csv");
       const csvText = await res.text();
       const parsed = parseCSVText(csvText);
 
@@ -19,12 +21,22 @@ export default function ExcelTest() {
       console.error("Failed to fetch or parse CSV", err);
     }
   };
+  function ColorButton() {
+  const [clicked, setClicked] = useState(false);
+
+  return (
+    <button
+      onClick={() => handleTestStaticCSV()}
+    >
+      Load
+    </button>
+  );
+};
 
   return (
     <div>
       <h2>Test Static CSV File</h2>
-      <button onClick={handleTestStaticCSV}>Load /tdataset/data_dyes.csv</button>
-
+      <ColorButton />
       {parsedData && (
         <div>
           <h3>Parsed {parsedData.length} entries</h3>

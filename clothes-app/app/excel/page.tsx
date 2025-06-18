8b13e49d559785c08e581496c4e7e7a72e0e2a47
Rémy Fayet <rems64des@gmail.com>
2025-06-18 12:58:06 +0200

    add table and color preview

--- clothes-app/app/excel/page.tsx
+++ clothes-app/app/excel/page.tsx
@@ -1,51 +1,97 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
-import { parseCSVText, ColorEntry } from "../../components/color_handling"; // adjust path as needed
+import React, { ChangeEvent, useState } from 'react';
+import { parseCSVText, ColorEntry } from '../../components/color_handling'; // adjust path as needed
+import { Button } from '@/components/ui/button';
+import { FileIcon } from 'lucide-react';
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
+import { cn } from '@/lib/utils';
+import { ColorTranslator } from 'colortranslator';
 
+function ColorButton({ setParsedData }: { setParsedData: (colors: ColorEntry[]) => void }) {
+    const fileSubmit = async (e: ChangeEvent<HTMLInputElement>) => {
+        if (e?.target?.files && e.target.files.length > 0) {
+            const csvText = await e.target.files[0].text();
+            const parsed = parseCSVText(csvText);
+            setParsedData(parsed);
+        }
+    };
 
+    return (
+        <>
+            <Button variant="outline" asChild>
+                <label htmlFor="file-upload" className="cursor-pointer">
+                    <FileIcon /> Open File
+                </label>
+            </Button>
+            <input
+                id="file-upload"
+                type="file"
+                className="absolute inset-0 opacity-0 pointer-events-none"
+                onChange={fileSubmit}
+            />
+        </>
+    );
+}
 
 export default function ExcelTest() {
-  const [parsedData, setParsedData] = useState<ColorEntry[] | null>(null);
+    const [parsedData, setParsedData] = useState<ColorEntry[] | null>(null);
 
-  const handleTestStaticCSV = async () => {
-    try {
-      const res = await fetch("./dataset/data_dyes.csv");
-      const csvText = await res.text();
-      const parsed = parseCSVText(csvText);
-
-      setParsedData(parsed);
-
-      console.log("Parsed CSV data:", parsed);
-    } catch (err) {
-      console.error("Failed to fetch or parse CSV", err);
-    }
-  };
-  function ColorButton() {
-  const [clicked, setClicked] = useState(false);
-
-  return (
-    <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
-      onClick={() => handleTestStaticCSV()
-      
-      }
-    
-    >
-      Load
-    </button>
-  );
-};
-
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <h2>Test Static CSV File</h2>
-      <ColorButton />
-      {parsedData && (
-        <div>
-          <h3>Parsed {parsedData.length} entries</h3>
-          <pre>{JSON.stringify(parsedData, null, 2)}</pre>
+    return (
+        <div className="flex flex-col items-center justify-center min-h-dvh w-auto p-4 space-y-4">
+            <div className="space-y-2">
+                <h1 className="font-bold">Open your CSV file containing colors</h1>
+                <div>
+                    <ColorButton setParsedData={setParsedData} />
+                </div>
+            </div>
+            {parsedData && (
+                <div className="w-[75%]">
+                    <Table>
+                        <TableCaption>List of colors</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[10px]">Id</TableHead>
+                                <TableHead>Hours</TableHead>
+                                <TableHead>L</TableHead>
+                                <TableHead>a</TableHead>
+                                <TableHead>b</TableHead>
+                                <TableHead>e</TableHead>
+                                <TableHead>preview</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {parsedData.map((d, i) => {
+                                const col = new ColorTranslator({ L: d.L, a: d.a, b: d.b });
+                                return (
+                                    <TableRow key={i}>
+                                        <TableCell className="w-[10px]">{d.id}</TableCell>
+                                        <TableCell>{d.hours}</TableCell>
+                                        <TableCell>{d.L}</TableCell>
+                                        <TableCell>{d.a}</TableCell>
+                                        <TableCell>{d.b}</TableCell>
+                                        <TableCell>{d.E}</TableCell>
+                                        <TableCell>
+                                            <div
+                                                className={cn('h-[2rem] w-[2rem] rounded')}
+                                                style={{ backgroundColor: col.RGB }}
+                                            ></div>
+                                        </TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }

5683e86921ff9d74441abda28ea9559f132f0e8b
Rémy Fayet <rems64des@gmail.com>
2025-06-19 01:07:32 +0200

    wip

--- pbr_texture_generation/src/drawable.rs
+++ pbr_texture_generation/src/drawable.rs
@@ -2,6 +2,7 @@ use nalgebra::{Point2, Vector3};
 
 pub trait Drawable {
     fn get_height(&self, point: Point2<f32>) -> f32;
+    fn get_height_with_id(&self, point: Point2<f32>) -> (f32, usize);
     fn get_normal(&self, point: Point2<f32>) -> Vector3<f32>;
     fn get_albedo(&self, point: Point2<f32>) -> Vector3<f32>;
 }
